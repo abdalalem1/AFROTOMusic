56e9c4e89f49d3daf8ceafa9b26e68d72313b9a4
abdalalem1 <165777016+abdalalem1@users.noreply.github.com>
2024-04-02 17:38:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abdalaleK/AFROTOMusic",
+    "https://github.com/abdalalem1/AFROTOMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
